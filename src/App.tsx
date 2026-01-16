1234dc4dbdc1016fc9cbc498dbaa71d82db3ffa8
Divya-Darshan <parukrish.darshan20@gmail.com>
2026-01-16 15:43:54 +0530

    inline CSS -->  external CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <p>Start prompting (or editing) to see magic happen :)</p>
-    </div>
+    // <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    //   <p>Start prompting (or editing) to see magic happen :)</p>
+    // </div>
+    <>
+    // I don't know whoever you are seeing this next after me that thing 👆 about there was not made by me it was already there it was doing nothing and I just commented down 
+    // it might be ai or something but I don't know
+    </>
+
   );
 }
 
